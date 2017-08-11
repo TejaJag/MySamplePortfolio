c3838924aa6c1592ea26cf54f791a2172574be29
teja <tejajagarlamudi95@gmail.com>
2017-08-10 21:16:06 -0500

    initalized the proj with yeoman. grunt serve works with some warnings of compass.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tejaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tejaApp
+ */
+angular.module('tejaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
