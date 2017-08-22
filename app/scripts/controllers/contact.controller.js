d8d25dfdcb5471c97d96dd8be855b1d168c92508
teja <tejajagarlamudi95@gmail.com>
2017-08-22 15:00:35 -0500

    added sample map

--- app/scripts/controllers/contact.controller.js
+++ app/scripts/controllers/contact.controller.js
@@ -8,11 +8,20 @@
  * Controller of the tejaApp
  */
 angular.module('appApp')
-  .controller('contactCtrl', function ($scope) {
+  .controller('contactCtrl', function ($scope, NgMap) {
     let vm = this;
     console.log("hell contact");
     $scope.contactDetails = {};
     $scope.contactDetails.name = "Tejaswini J";
     $scope.contactDetails.email = "tejajagarlaudi95@gmail.com";
+    $scope.x = 42.74 ;
+    $scope.y = -76.18 ;
+//  AIzaSyBnbRbHgXcER3afS28Bm3of2dz4MEBEhjo  -- google maps API key
+    NgMap.getMap().then(function(map) {
+    // console.log(map.getCenter());
+    // console.log('markers', map.markers);
+    // console.log('shapes', map.shapes);
+    console.log("maps");
+  });
 
   });
